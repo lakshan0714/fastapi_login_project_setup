ce5aa3dcc7d256f565145942397994c4b55b40cc
lakshan0714 <131190368+lakshan0714@users.noreply.github.com>
2025-10-24 18:44:58 +0530

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -4,7 +4,7 @@ from src.config.database import Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
-from src.schemas.UserSchemas import UserRole
+from src.schemas.user_schemas import UserRole
 
 
 class User(Base):
